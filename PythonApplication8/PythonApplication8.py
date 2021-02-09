bd93531ee5cd7fd140658bb79fb1ee4f73403689
DariaEvtina <77333298+DariaEvtina@users.noreply.github.com>
2021-02-09 08:57:45 +0200

    Update PythonApplication8.py

--- PythonApplication8/PythonApplication8.py
+++ PythonApplication8/PythonApplication8.py
@@ -4,9 +4,6 @@ from tkinter import scrolledtext
 from tkinter.messagebox import*
 from random import*
 
-
-
-
 mas2=[]
 f=open("wordsa.txt",'r',encoding="utf-8-sig")
 for l in f:
@@ -43,7 +40,7 @@ def add_words():
  f_.write(mas1[len(mas1)-1]+"\n")
  f_.close()
         #lb.configure(text="If you need to simultaneously add several words to the dictionary,\n enter the word and its translation separated by commas ','.\n(example:небо,sky)",bg="lightslategrey")
-
+a=0
 def tr2():
  l1_=str(l1.get())
  if l1_ in mas1:
@@ -54,57 +51,21 @@ def tr2():
   l2.configure(text=f"{mas1[ide]}")
  else:
    l2.configure(text="в словаре нет этого слова")
-
-def del_():
-    global eb
-    eb_=(eb[0,END].strip())
-    for word in mas1:
-     if word.find(eb_)==1:
-      lk=Label(tab3,text="correct the word:",font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=24)
-      p=Entry(tab3,font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=25)
-      lk.grid(row=3,column=0)
-      p.grid(row=3,column=1)
-      ind=mas1.index(eb_)
-      mas1.pop(ind)
-      mas1.insert(ind,str(p))
-      fail=open("wordsa.txt",'w',encoding="utf-8-sig")
-      for i in r :
-       fail.write(i+"\n")
-      fail.close()
-     elif word.find(eb_)!=1:
-      for word in mas2:
-       if word.find(eb_)==1:
-        lk=Label(tab3,text="correct the word:",fg="ghostwhite",bg="lightslategrey",width=25)
-        p=Entry(tab3,font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=25)
-        lk.grid(row=3,column=0)
-        p.grid(row=3,column=1)
-        a=str(p)
-        ind=mas2.index(eb_)
-        mas2.pop(ind)
-        mas2.insert(ind,a)
-        fail=open("wordas.txt",'w',encoding="utf-8-sig")
-        for i in mas2:
-         fail.write(i+"\n")
-        fail.close()
-       else:
-        l=Label(tab3,text=" слова нет в программе",font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=25)
-        l.grid(row=3,column=2)
-     else:
-       l=Label(tab3,text=" слова нет в программе",font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=25)
-       l.grid(row=4,column=2)
-
+def testing():
+ global a
+ a+=1
 def result():
-    global name
+    global name,a
     if name.get()!=" ":
      fail_=open("Result.txt","a",encoding="utf-8-sig")
-     fail_.write(name+","+)
+     fail_.write(name+","+f"5:{a}")
+     fail_.close()
 def test():
  win1=Tk()
  win1.geometry("300x400")
  win1.title("TEST")
  e=0
  e1=1
-
  for i in range(5):
   r=randint(0,(len(mas1)-1))
   r_=randint(0,(len(mas1)-1))
@@ -115,18 +76,18 @@ def test():
       pass
   random_=randint(0,2)
   if random_==2:
-      r3=Radiobutton(win1,text=f"{mas1[r]}",font="Calibri 10",fg="ghostwhite",width=5)
+      r3=Radiobutton(win1,text=f"{mas1[r]}",font="Calibri 10",fg="ghostwhite",width=5,command=testing)
       r2=Radiobutton(win1,text=f"{mas1[r_]}",font="Calibri 10",fg="ghostwhite",width=5)
       r1=Radiobutton(win1,text=f"{mas1[r_1]}",font="Calibri 10",fg="ghostwhite",width=5)
   elif random_==1:
       r3=Radiobutton(win1,text=f"{mas1[r_1]}",font="Calibri 10",fg="ghostwhite",width=5)
-      r2=Radiobutton(win1,text=f"{mas1[r]}",font="Calibri 10",fg="ghostwhite",width=5)
+      r2=Radiobutton(win1,text=f"{mas1[r]}",font="Calibri 10",fg="ghostwhite",width=5,command=testing)
       r1=Radiobutton(win1,text=f"{mas1[r_]}",font="Calibri 10",fg="ghostwhite",width=5)
   else:
       r3=Radiobutton(win1,text=f"{mas1[r_]}",font="Calibri 10",fg="ghostwhite",width=5)
       r2=Radiobutton(win1,text=f"{mas1[r_1]}",font="Calibri 10",fg="ghostwhite",width=5)
       r1=Radiobutton(win1,text=f"{mas1[r]}",font="Calibri 10",fg="ghostwhite",width=5,command=testing)
-  labele=Label(win1,text=f"{mas2[r]}-???",font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=5)
+  labele=Label(win1,text=f"{mas2[r]}-???",font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=15)
   labele.grid(row=e,column=0)
   r1.grid(row=e1,column=0)
   r2.grid(row=e1,column=1)
@@ -135,7 +96,6 @@ def test():
   e1+=2
  name=Entry(win1,font="Calibri 14",fg="ghostwhite",bg="lightslategrey",width=24)
  name.grid(row=5,column=0)
- 
  win1.mainloop()
 #############################################################################################################################################
 
@@ -173,9 +133,7 @@ btns.grid(row=0,column=2)
 l7=Label(tab3,text="eng/rus",font="Calibri 22",fg="ghostwhite",bg="lightslategrey",width=10)
 o=Entry(tab3,font="Calibri 22",fg="ghostwhite",bg="lightslategrey",width=25)
 btn1=Button(tab3,text="Add new words in dictionary",font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=25,command=add_words)
-vb=Label(tab3,text="enter the word you want to correct: ",font="Calibri 14",fg="ghostwhite",bg="lightslategrey",width=30)
-eb=Entry(tab3,font="Calibri 14",fg="ghostwhite",bg="lightslategrey",width=24)
-b=Button(tab3,text="delete",font="Calibri 10",fg="ghostwhite",bg="lightslategrey",width=24,command=del_)
+
 
 l7.grid(row=0,column=0)
 o.grid(row=0,column=1)
